0259b5707730944328bded78a0dc1c740319b305
Muhammad Abdullah <whello544@gmail.com>
2026-07-10 21:40:07 +0500

    Rename 'Price_Per_Cup' to 'Price'
    Renamed 'Price_Per_Cup' column to 'Price' in coffee DataFrame.

--- practise_2.py
+++ practise_2.py
@@ -7,3 +7,10 @@ coffee.columns
 # droping column 
 coffee = coffee.drop("Discount", axis=1)
 coffee.head()
+
+# rename column
+coffee = coffee.rename(columns={
+    "Price_Per_Cup" : "Price"
+})
+coffee.head()
+
